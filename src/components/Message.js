5beaf09593f932ba99f31d5e43e6c265de3778b1
casinca4 <s.antonelli@web.de>
2020-01-10 14:26:01 +0100

    Minor changes

---
+++ src/components/Message.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <section className="message">
+          <form className="input-message" onSubmit={this.handleSubmit}>
+            <label>
+              <input class="text-input" type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input class="send-button" type="submit" value="SEND" />
+          </form>
+          <div className="participants">
+            <p id="you">YOU</p>
+            <p id="friend">YOUR IMAGINARY FRIEND</p>
+          </div>
+      </section>
+    );
+  }
+}
+
+export default Message;
